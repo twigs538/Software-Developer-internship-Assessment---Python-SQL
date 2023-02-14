68926ca74772ed8a26ccccb6e71135cf4d034b1d
twigs538 <62210934+twigs538@users.noreply.github.com>
2023-02-14 09:00:59 +0200

    Delete Round I directory

--- Round I/myproject/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.get_params),
-    path('get_params', views.get_params),
-]
--- Round I/question_2.py
+++
@@ -1,6 +0,0 @@
-for i in range(1, 11):
-    if i==6:
-    #your code here#
-        pass
-    else:
-        print(i, end=',')
